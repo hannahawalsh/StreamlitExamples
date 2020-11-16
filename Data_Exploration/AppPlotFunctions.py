c04bf1189f1a2d78143378f1092d39de7316f1ea
Hannah Morgan <34978368+hannahawalsh@users.noreply.github.com>
2020-11-16 17:25:12 -0600

    Add files via upload

---
+++ Data_Exploration/AppPlotFunctions.py
@@ -0,0 +1,155 @@
+"""
+
+"""
+import altair as alt
+
+
+
+def create_grouped_kde(df, col, target=None, by_class=True, size=175,
+                       ylabel=None, xlabel=None):
+    """ 
+    Create an altair chart showing the kernel density estimate of a variable 
+    with the option to plot a separate distribution by each class. 
+    
+    Parameters 
+    ----------
+    df : pandas DataFrame 
+    col : string
+        The column of df to be plotted 
+    target : string
+        The column of df that holds target labels 
+    by_class : boolean, default True 
+        Whether to plot data by class 
+    size : integer 
+        Size (width & height) of the returned plot 
+    """
+    ylabel = ylabel if ylabel is not None else "density"
+    xlabel = xlabel if xlabel is not None else col
+    if by_class:
+        labels = df[target].unique()
+        colors = ["DarkMagenta", "MediumOrchid", "RebeccaPurple"]
+        chart = alt.Chart(df, title=col).transform_density(
+            density=col, counts = True, groupby=[target],
+            steps=len(df),extent=[df[col].min() * 0.8,
+            df[col].max() * 1.2], as_=[col, "density"]
+        ).mark_area(
+            opacity=0.7, 
+            line=alt.OverlayMarkDef(stroke="black", strokeWidth=3)
+        ).encode(
+            x=alt.X(f"{col}:Q", title=xlabel),
+            y=alt.Y("density:Q", title=ylabel),
+            color=alt.Color(f"{target}:N", scale=alt.Scale(domain=labels,
+                            range=colors[:len(labels)]))
+        )
+    else:
+        chart = alt.Chart(df, title=col).transform_density(
+            density=col, counts = True, steps=len(df),
+            extent=[df[col].min() * 0.8, df[col].max() * 1.2],
+            as_=[col, "density"]
+        ).mark_area(
+            opacity=0.8, 
+            line=alt.OverlayMarkDef(stroke="black",
+                 strokeWidth=3, fill="DarkMagenta")
+        ).encode(
+            x=alt.X(f"{col}:Q", title=xlabel),
+             y=alt.Y("density:Q", title=ylabel)
+        )
+    return chart.properties(width=size, height=size)
+
+
+def create_distribution_figure(df, by_class, size=175):
+    plot_rows = alt.vconcat(data=df)
+    n_cols = 3
+    n_rows = (len(df.columns) - 1) // n_cols + 1
+
+    for i in range(0, len(df.columns), n_cols):
+        current_row = df.columns[i: i+n_cols]
+        plot_cols = alt.hconcat()
+        
+        for df_col in current_row:
+            if df_col == "target":
+                trg = df.target.value_counts().reset_index().rename(
+                            columns={"target": "count", "index": "label"})
+                cht = alt.Chart(trg).mark_bar(stroke="black",
+                                 strokeWidth=3).encode(
+                          x=alt.X("label:O", title="target"), 
+                          y=alt.Y("count:Q", title="Count"), 
+                          color="label:O",
+                          tooltip=["label", "count"]
+                      ).properties(width=size, height=size, title="target")
+                plot_cols |= cht 
+            else:
+                plot_cols |= create_grouped_kde(df, df_col, "target", by_class,
+                                                size=size)
+        plot_rows &= plot_cols
+    chart = plot_rows.configure_legend(
+            orient="top", titleFontSize=10,labelFontSize=10
+        ).configure_title(fontSize=14, anchor="middle"
+        ).configure_axis(grid=False, labelAngle=0
+        )
+    
+    return chart
+
+
+def create_comparison_figure(df, cols, by_class, size=100):
+    plot_rows = alt.vconcat(data=df)
+    labels = df["target"].unique()
+    colors = ["DarkMagenta", "MediumOrchid", "RebeccaPurple"]
+    plot_colors = alt.Color(f"target:N", scale=alt.Scale(domain=labels,
+                                range=colors[:len(labels)]))
+    for i, col in enumerate(cols):
+        plot_cols = alt.hconcat()        
+        for j, secondary in enumerate(cols[:i+1]):
+            ylabel = col if j == 0 else ""
+            xlabel =  secondary if i == len(cols) - 1 else ""
+            if secondary == col:
+                cht = create_grouped_kde(df, col, "target", by_class=by_class,
+                                         size=size, ylabel=ylabel,
+                                         xlabel=xlabel)
+                cht = cht.properties(title="")
+            else:
+                cht = alt.Chart(df).mark_circle(size=50
+                    ).encode(x=alt.X(f"{secondary}:Q", title=xlabel), 
+                             y=alt.Y(f"{col}:Q", title=ylabel),
+                             color=plot_colors
+                    ).properties(width=size, height=size)
+            plot_cols |= cht
+        plot_rows &= plot_cols
+        
+    chart = plot_rows.configure_axis(
+            grid=False, titleFontSize=12
+        ).configure_title(
+        ).configure_legend(orient="top", titleFontSize=20, labelFontSize=16
+        )
+    if not by_class:
+        return chart.configure_mark(color="RebeccaPurple")
+    return chart
+
+
+def create_corrolation_plot(data):
+    corr = data.corr().reset_index().melt(id_vars="index")
+    corr.columns = ["Variable 1", "Variable 2", "corr_values"]
+    corr["Correlation"] = corr.corr_values.round(3)
+    
+    cht = alt.Chart(corr).encode(
+            x="Variable 1:N", y="Variable 2:N"
+        ).properties(title="Correlation Plot", width=700, height=700)
+    txt = cht.mark_text().encode(
+            text="Correlation", 
+            color=alt.condition(
+                alt.datum.Correlation > 0.25, 
+                if_true=alt.value('white'),
+                if_false=alt.value('black')
+        ))
+    rct = cht.mark_rect().encode(
+            color=alt.Color("corr_values:Q", scale=alt.Scale(scheme="purples"),
+                            legend=None),
+            tooltip=["Variable 1", "Variable 2", "Correlation"]
+        )
+    cht = rct + txt
+    cht = cht.configure_axis(
+            ticks=False, title=None, labelPadding=10, labelFontSize=14
+        ).configure_title(
+            fontSize=20, anchor="middle", dy=-20
+        )
+    return cht
---
+++ Data_Exploration/Wine_Data_App.py
@@ -0,0 +1,256 @@
+"""
+This is a simple app demonstrating some important and interesting features of
+streamlit. To run, type `streamlit run Data_Explorer.py` in your terminal.
+"""
+### Imports
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_wine
+
+from AppPlotFunctions import *
+
+
+
+### Main application function
+def main():
+    """
+    This is the main body of the app and is what is run when the script is
+    called from the terminal. It contains all the layout information and
+    executes the appropriate computations.
+    """
+    ### Load the data
+    data = load_data()
+    data_cols = list(data.columns)
+
+    ### Set up our app layout
+    # Main page title: HTML formatting
+    html_page_title = ("<h1 style='text-align: center; font-size: 3.0em; "
+                       "color: RebeccaPurple;'> Wine Data Exploration </h1>")
+    st.markdown(html_page_title, unsafe_allow_html=True)
+
+    # Main page: use containers
+    data_container = st.beta_container()
+    # description = st.beta_expander("Data Description", expanded=False)
+    filter_container = st.beta_expander("Data Filtering", expanded=False)
+    corrolation_map = st.beta_expander("Correlation Plot", expanded=True)
+    distribution_plot = st.beta_expander("Distribution Plot", expanded=True)
+    comparison_plot = st.beta_expander("Comparison Plot", expanded=True)
+    
+
+    description = st.beta_expander("Data Description", expanded=False)
+    description.write(return_description())
+
+    # Sidebar: use containers
+    display_options = st.sidebar.beta_container()
+    st.sidebar.write("---")
+    plot_options = st.sidebar.beta_container()
+
+
+    ### Add content to the app
+    ## To sidebar
+    display_options.title("Display Options")
+    colr_data = display_options.checkbox("Highlight data based on class")
+    # display_options.write("Sort by Column:")
+    sort_cont = display_options.beta_columns([4, 3])
+    sort_col = sort_cont[0].selectbox("Sort:", ["---"] + data_cols,
+                                      key="sort_by")
+    sort_cont[0].empty()
+    ascending = sort_cont[1].selectbox(" ­­", ["Low->High", "High->Low"],
+                                       key="sort_how")
+    num_rows = display_options.number_input("Number of Rows to Display", 1,
+                                            data.shape[0], 5)
+    plot_options.title("Plotting Options")
+    by_class = plot_options.checkbox("Show distribution by class", True)
+    plot_filtered = plot_options.checkbox("Plot filtered data", True)
+    plot_options.write("Select 4 Features to Compare:")
+    plot_vars = get_plot_vars()[-4:]
+    plot_vars.append(plot_options.selectbox("Feature 1", ["---"] + data_cols,
+                                  index=1, key="f1"))
+    plot_vars.append(plot_options.selectbox("Feature 2", ["---"] +
+                     exclude(data_cols, plot_vars), index=1, key="f2"))
+    plot_vars.append(plot_options.selectbox("Feature 3", ["---"] +
+                     exclude(data_cols, plot_vars), index=1, key="f3"))
+    plot_vars.append(plot_options.selectbox("Feature 4", ["---"] +
+                     exclude(data_cols, plot_vars), index=1, key="f4"))
+
+    ## To the main page
+    #  Filters
+    filter_container.markdown("##### Add a new filter:")
+    filter_cols = filter_container.beta_columns([2, 1, 1])
+    col_filter = filter_cols[0].selectbox("", data_cols)
+    filter_exp = filter_cols[1].selectbox("", ["<", "==", ">"], index=2)
+    filter_num = filter_cols[2].number_input("", data[col_filter].min(),
+                    data[col_filter].max(), data[col_filter].mode()[0])
+    add_filter = filter_container.button("Add filter")
+
+    current_filters = filter_container.beta_columns(2)
+    current_filters[0].markdown("#### Staged filters:")
+    staged_text = current_filters[0].empty()
+    apply_filters = current_filters[0].button("Apply Filters")
+    current_filters[1].markdown("#### Active filters:")
+    active_text = current_filters[1].empty()
+    rm_filters = current_filters[1].button("Remove all filters")
+
+    # Retrieve all set filters and the previously filtered frame
+    active_filters = get_filters()
+    filtered_data = get_filtered_data()
+    filtered =  (filtered_data["data"] if filtered_data["data"] is not None
+                 else data.copy())
+
+    # Add new filter button
+    if add_filter:
+        if col_filter in active_filters:
+            subdict = active_filters[col_filter]
+            active_filters.update({col_filter: get_filter_update(subdict,
+                                  filter_exp, filter_num)})
+        else:
+            subdict = {"<": None, ">": None, "==": None, "active": False}
+            subdict[filter_exp] = filter_num
+            active_filters.update({col_filter: subdict})
+
+    # Apply filters button
+    if apply_filters:
+        queries = []
+        for col, filter_dict in active_filters.items():
+            queries.append(get_filter_expression(filter_dict, col))
+            active_filters[col].update({"active": True})
+        if queries:
+            filtered = filtered.query(" & ".join(queries))
+
+    # Remove filters button
+    if rm_filters:
+        active_filters.clear()  # clears the cache
+        filtered = data.copy()
+
+    # Display what filters have been "staged" and which ones are "active"
+    staged_text.markdown("\---")
+    active_text.markdown("\---")
+    if active_filters:
+        staged = [get_filter_expression(filter_dict, col) for col, filter_dict
+                  in active_filters.items() if "active" in filter_dict.keys()
+                  and filter_dict["active"] == False]
+        active = [get_filter_expression(filter_dict, col) for col, filter_dict
+                  in active_filters.items() if "active" in filter_dict.keys()
+                  and filter_dict["active"] == True]
+        if staged:
+            staged_text.markdown("  \n".join(staged))
+        if active:
+            active_text.markdown("  \n".join(active))
+
+    # Cache the newly filtered frame
+    filtered_data.update({"data": filtered})
+
+    # Display the filtered & styled data
+    Style.num_rows = num_rows
+    Style.colored = True if colr_data else False
+    Style.sort_column = sort_col
+    Style.ascending = True if ascending == "Low to High" else False
+    data_container.write(f"Dataframe contains {filtered.shape[0]} data points")
+    # data_container.table(Style().style(filtered))
+    data_container.table(Style().style(filtered))
+
+    # Plots
+    plot_df = filtered if plot_filtered else data
+    corrolation_map.altair_chart(create_corrolation_plot(plot_df),
+                                 use_container_width=True)
+    distribution_plot.altair_chart(create_distribution_figure(plot_df,
+                                                              by_class))
+    compare_cols = [v for v in plot_vars if v != "---"]
+    if compare_cols:
+        comparison_plot.altair_chart(create_comparison_figure(plot_df,
+                                     compare_cols, by_class, size=120))
+    
+
+
+### Cached functions
+@st.cache(allow_output_mutation=True)
+def load_data():
+    data = load_wine(as_frame=True).frame
+    return data.sample(frac=1, replace=False,
+                       random_state=101).reset_index(drop=True)
+
+@st.cache(allow_output_mutation=True)
+def return_description():
+    return load_wine().DESCR[19:]
+
+@st.cache(allow_output_mutation=True)
+def get_plot_vars():
+    return []
+
+@st.cache(allow_output_mutation=True)
+def get_filters():
+    return {}
+
+@st.cache(allow_output_mutation=True)
+def get_filtered_data():
+    return {"data": None}
+
+### Plot functions
+
+
+
+### Other Functions
+def exclude(array, exclude_things):
+    if not isinstance(exclude_things, list):
+        exclude_things = [exclude_things]
+    return [x for x in array if x not in exclude_things]
+
+def get_filter_update(subdict, filter_exp, filter_num):
+    """ Gets the update to the filter dictionary """
+    if filter_exp == "==":
+        subdict = {"<": None, ">": None, "==": filter_num, "active": False}
+    else:
+        subdict.update({filter_exp: filter_num, "==": None, "active": False})
+        other = "<" if filter_exp == ">" else ">"
+        if subdict[">"] and subdict["<"] and (subdict[">"] >  subdict["<"]):
+            subdict[other] = None
+    return subdict
+
+def get_filter_expression(filter_dict, col):
+    defined = [k for k, v in filter_dict.items() if v and k!="active"]
+    return " and ".join([f"{col} {k} {round(filter_dict[k], 2)}" for
+                         k in defined])
+
+
+### Style class to display dataframe
+class Style:
+    num_rows = 3
+    colored = False
+    sort_column = "---"
+    ascending = True
+
+    def color_class(self, row):
+        """ Color each row according to it's target variable """
+        colors = ["DarkMagenta", "MediumOrchid", "RebeccaPurple"]
+        return [f"background-color: {colors[int(row.target)]}"]*len(row)
+
+
+    def style(self, df):
+        """ Apply all the styles to a dataframe df """
+        ### Change Style
+        # Display 2 decimal places
+        if Style.sort_column != "---":
+            df = df.sort_values(by=Style.sort_column,
+                                ascending=Style.ascending)
+        st_df = df.head(Style.num_rows).style.set_precision(2)
+        font_color = "black"
+        if Style.colored == True:
+            st_df = st_df.apply(self.color_class, axis="columns")
+            font_color = "white"
+
+        return st_df.set_properties(**{"color": font_color})
+
+
+
+if __name__ == "__main__":
+    ### Set the color palette
+    colors = ["DarkMagenta", "MediumOrchid", "RebeccaPurple", "DarkOrchid",
+              "DarkViolet", "BlueViolet", "Indigo", "MediumPurple", "Purple"]
+    wine_palette = sns.color_palette(colors)
+    sns.set_palette(wine_palette)
+
+    main()
